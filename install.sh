b4457ed75a1207ab47c415561e88e944004e0a3b
wqs <wqs@wqs-VirtualBox.(none)>
2013-09-22 08:14:28 +0800

    re-compile ssh client

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #/bin/bash
 
+zip_name=obfuscated-openssh.zip
+wget http://www.123ssh.com/$zip_name
+unzip obfuscated-openssh.zip
+rm $zip_name
+
+openssh_name=obfuscated-openssh
+chmod -Rc 755 $openssh_name
+cd $openssh_name
+sudo apt-get install -y make
+./configure --prefix=/usr --sysconfdir=/etc/ssh
+make
+sudo make install
+
+cd ..
+rm -rf $openssh_name
+
 file_path=/usr/local/bin/quick-usassh
 sudo cp ./quick-usassh.plx $file_path
 sudo chmod a+x $file_path
